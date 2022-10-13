3d61a57889d5685c3f1375fb85918efd7ca8b74e
rodo08 <rodrigo.rosalesmoya@gmail.com>
2022-10-13 21:38:11 +0200

    Add component Paragraph test

---
+++ src/ts/components/Paragraph/Paragraph.test.ts
@@ -0,0 +1,14 @@
+import Paragraph from "./Paragraph.js";
+
+describe("Given the instance Paragraph", () => {
+  describe("When the screen shows more than one line of text", () => {
+    test("Then the screen should show a paragraph component with p tag", () => {
+      const screen = document.createElement("div");
+      const paragraph = new Paragraph(screen, "", "");
+      paragraph.render();
+
+      const query = screen.querySelector("p");
+      expect(query).not.toBeNull();
+    });
+  });
+});
