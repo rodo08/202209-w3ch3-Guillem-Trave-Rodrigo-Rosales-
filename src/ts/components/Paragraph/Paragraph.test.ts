1848d6f5e809013fe8f1faa1b152347687f3f31f
Tutitoos <tutitoosdev@gmail.com>
2022-10-14 09:18:21 +0200

    Refactor selectors tag for className and text content

--- src/ts/components/Paragraph/Paragraph.test.ts
+++ src/ts/components/Paragraph/Paragraph.test.ts
@@ -2,15 +2,17 @@ import Paragraph from "./Paragraph.js";
 
 describe("Given the instance Paragraph", () => {
   describe("When the screen shows more than one line of text", () => {
-    test("Then the screen should show a paragraph component with p tag", () => {
+    test("Then the screen should show a paragraph component with p tag and content 'You have not watched any serie yet'", () => {
+      const title = "You have not watched any serie yet";
       const screen = document.createElement("div");
 
-      const paragraph = new Paragraph(screen, "", "");
+      const paragraph = new Paragraph(screen, "info", title);
       paragraph.render();
 
-      const query = screen.querySelector("p");
+      const query = screen.querySelector(".info");
 
       expect(query).not.toBeNull();
+      expect(query.textContent).toBe(title);
     });
   });
 });
